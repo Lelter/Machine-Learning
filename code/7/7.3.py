b4f28ee1d81fb6afb0b4cf2f928e06fa779cb8c5
vice-computer <1067049538@qq.com>
2022-04-19 15:05:28 +0800

    1

--- code/7/7.3.py
+++ code/7/7.3.py
@@ -3,7 +3,11 @@ import pandas as pd
 from sklearn.utils.multiclass import type_of_target
 from collections import namedtuple
 
-
+def values_counts(x):
+    unique_value = x.unique()#取值情况
+    x_value_count = pd.value_counts(x)#计算每个取值的数量，并且加1，即拉普拉斯平滑
+    x_value_count = x_value_count[unique_value].fillna(0) + 1#拉普拉斯平滑
+    return x_value_count
 def train_nb(X, y):
     m, n = X.shape
     p1 = (len(y[y == '是']) + 1) / (m + 2)  # 拉普拉斯平滑
@@ -23,34 +27,33 @@ def train_nb(X, y):
             X.columns[i], ['is_continuous', 'conditional_pro'])  # 用于储存每个变量的情况
 
         is_continuous = type_of_target(xi) == 'continuous'
-        xi1 = X1.iloc[:, i]
-        xi0 = X0.iloc[:, i]
+        xi1 = X1.iloc[:, i]#正例下的每个特征列表
+        xi0 = X0.iloc[:, i]#负例下的每个特征列表
         if is_continuous:  # 连续值时，conditional_pro 储存的就是 [mean, var] 即均值和方差
-            xi1_mean = np.mean(xi1)
-            xi1_var = np.var(xi1)
+            xi1_mean = np.mean(xi1)#求均值
+            xi1_var = np.var(xi1)#求方差
             xi0_mean = np.mean(xi0)
             xi0_var = np.var(xi0)
 
-            p1_list.append(p_xi(is_continuous, [xi1_mean, xi1_var]))
-            p0_list.append(p_xi(is_continuous, [xi0_mean, xi0_var]))
+            p1_list.append(p_xi(is_continuous, [xi1_mean, xi1_var]))#训练集中正例的每个属性的条件概率,
+            p0_list.append(p_xi(is_continuous, [xi0_mean, xi0_var]))#训练集中负例的每个属性的条件概率
         else:  # 离散值时直接计算各类别的条件概率
             unique_value = xi.unique()  # 取值情况
             nvalue = len(unique_value)  # 取值个数
 
-            xi1_value_count = pd.value_counts(xi1)[unique_value].fillna(
-                0) + 1  # 计算正样本中，该属性每个取值的数量，并且加1，即拉普拉斯平滑
-            xi0_value_count = pd.value_counts(xi0)[unique_value].fillna(0) + 1
+            xi1_value_count = values_counts(xi1)  # 计算正样本中，该属性每个取值的数量，并且加1，即拉普拉斯平滑
+            xi0_value_count = values_counts(xi0)  # 计算负样本中，该属性每个取值的数量，并且加1，即拉普拉斯平滑
 
-            p1_list.append(p_xi(is_continuous, np.log(
+            p1_list.append(p_xi(is_continuous, np.log(# 计算正样本中，该属性每个取值的条件概率
                 xi1_value_count / (m1 + nvalue))))
-            p0_list.append(p_xi(is_continuous, np.log(
+            p0_list.append(p_xi(is_continuous, np.log(# 计算负样本中，该属性每个取值的条件概率
                 xi0_value_count / (m0 + nvalue))))
 
     return p1, p1_list, p0_list
 
 
 def predict_nb(x, p1, p1_list, p0_list):
-    n = len(x)
+    n = len(x)#特征个数
 
     x_p1 = np.log(p1)
     x_p0 = np.log(1 - p1)
@@ -77,8 +80,8 @@ def predict_nb(x, p1, p1_list, p0_list):
 
 if __name__ == '__main__':
     data_path = r'E:\Machine-Learning\code\dataset\3.0.csv'
-    data = pd.read_csv(data_path, index_col=0)
-
+    data = pd.read_csv(data_path)
+    del data['编号']
     X = data.iloc[:, :-1]
     y = data.iloc[:, -1]
     p1, p1_list, p0_list = train_nb(X, y)
